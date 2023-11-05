d5b24fabb8645677ae6f97f03d03234ce79f21b1
Bassem Debbabi <bassem.debbabi@atos.net>
2023-11-05 23:34:04 +0100

    Add a greet_user function to main.py

--- main.py
+++ main.py
@@ -3,5 +3,8 @@
 def main():
     print("Welcome to the Git Branching and Merging Tutorial!")
 
+def greet_user(username):
+    print(f"Hello, {username}! We hope you enjoy the tutorial!")
+
 if __name__ == "__main__":
     main()
